7d56d95ab31fa7b72a5176c96c6b703d0abeec6d
Aviad_Hemo <aviad@herolo.co.il>
2019-05-04 10:52:48 +0300

    fix: fix extension crash when target folder is empty

--- src/extension.ts
+++ src/extension.ts
@@ -6,15 +6,20 @@ import { FileService } from './files-service/file.service';
 
 export function activate(context: ExtensionContext) {
 	let disposable = commands.registerCommand('extension.indexerator', (fullPath) => {
-		const folderName = relative(process.cwd(), fullPath.fsPath).split(sep);
-		window.setStatusBarMessage(`indexerator >index in ${folderName[folderName.length - 1]}`);
 		const fileService = new FileService();
 
-		try {
+		fileService.on('error', () => {
+				window.showErrorMessage(`Unable to create ${fileService.indexFileName}`)
+			})
+			.on('create', () => {
+				const folderName = relative(process.cwd(), fullPath.fsPath).split(sep);
+				window.setStatusBarMessage(`indexerator >index in ${folderName[folderName.length - 1]}`);
+
+				// clear status bar after 5 sec
+				setTimeout(() => window.setStatusBarMessage(''), 5 * 1000);
+			});
+
 			fileService.generateExportFile(fullPath.fsPath);
-		} catch(err) {
-			window.setStatusBarMessage('indexerator failed create file');
-		}
 	});
 
 	context.subscriptions.push(disposable);
