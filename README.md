2b2d4a079cf1a4b2d29d04f1c78692f3252b3bf2
Aviad_Hemo <aviad@herolo.co.il>
2019-05-04 18:26:50 +0300

    feature: added quote style setting and some error notifications

--- README.md
+++ README.md
@@ -2,8 +2,23 @@
 
 ## Features
 
-vscode index generator
-vscode extension that auto generate index.ts/index.js file that export all current folder
+* vscode index generator.  
+vscode extension that auto generate ```index.ts``` or ```index.js``` file that export all current folder
+
+File name can be change in settings.
+Example:
+```json
+"indexerator.exportFileName": "public_api"
+```
+
+- you can change the quote style in the settings- default style is single quote
+```json
+"indexerator.quote" : "double"
+```
+or 
+```json
+"indexerator.quote" : "single"
+```
 
 ```
 project
@@ -44,11 +59,13 @@ Right click on a folder in your project and you'll get the 'indexerator' option
 
 ## Known Issues
 
-No bugs, feel free to tell me about bugs.
+No bugs, feel free to report on bugs or create a PR that fix it.
 
 ## Release Notes
 
 Fixed- Extension crashes on empty folders.
+
+[Changelog](CHANGELOG.md)
 
 created by Aviad Hemo.
 
--- src/files-service/file.service.ts
+++ src/files-service/file.service.ts
@@ -1,50 +1,68 @@
-import { PathLike, readdir, writeFile } from "fs";
-import { extname } from 'path';
-import { window } from 'vscode';
+import { readdir, writeFile } from "fs";
+import { extname, relative, sep } from 'path';
 import { EventEmitter } from 'events';
+
+export type QuoteType = 'single' | 'double';
 
 export enum Extensions {
     ts = 'ts',
     js = 'js'
 }
 
+export enum Quotes {
+    single = '\'',
+    double = '\"'
+}
+
 export class FileService {
     private readonly indexName: string;
-    private readonly prefix = `export * from './`;
     private readonly eventEmitter: EventEmitter;
+    private prefix = `export * from './`;
+    private _quoteStyle: Quotes = Quotes.single;
 
     constructor(fileName?: string) {
         this.indexName = fileName || 'index';
         this.eventEmitter = new EventEmitter();
     }
 
+    public setQuoteStyle(quoteStyle: QuoteType) {
+        if(quoteStyle) {
+            this._quoteStyle = Quotes[quoteStyle] || Quotes.single;
+        }
+
+        this.prefix = this.prefix.replace(/'|"/, this._quoteStyle);
+    }
+
     get indexFileName(): string {
         return this.indexName;
     }
 
-    public generateExportFile(path: PathLike) {
+    public generateExportFile(path: string) {
         readdir(path, (err, files: string[]) => {
-            if(err) throw(err);
+            if(err) return this.emit('error', 'Unable to create file.');
 
             const foundedFile = files.find(file => file.endsWith(Extensions.js) || file.endsWith(Extensions.ts));
             if(!foundedFile) {
-                window.showErrorMessage('Files are not supported or the folder is empty');
+                if(files.length === 0) {
+                    return this.emit('error', 'can\'t create file in empty folder.');
+                }
 
-                return;
+                return this.emit('error', 'file extension is not supported.');
+                // window.showErrorMessage('Files are not supported or the folder is empty');
             }
 
             const extension = extname(foundedFile);
             const content = files.filter(file => file.endsWith(extension) && file !== `${this.indexName}${extension}`)
-            .map(file => `${this.prefix}${this.exportFileName(file)}';\n`).join('');
+            .map(file => `${this.prefix}${this.exportFileName(file)}${this._quoteStyle};\n`).join('');
 
             writeFile(`${path}/${this.indexName}${extension}`, content, (writreErr) => {
                 if(writreErr) {
-                    this.emit('error');
-                    
-                    return;
+                    return this.emit('error', `could'nt write file`);
                 }
 
-                this.emit('create');
+    			const folderName = relative(process.cwd(), path).split(sep);
+
+                this.emit('create', `indexerator >${this.indexFileName} in ${folderName[folderName.length - 1]}`);
             });
         });
     }
@@ -55,8 +73,8 @@ export class FileService {
         return this;
     }
 
-    private emit(event: 'create' | 'error'): void {
-        this.eventEmitter.emit(event);
+    private emit(event: 'create' | 'error', ...args: any[]): void {
+        this.eventEmitter.emit(event, args);
     }
 
 
